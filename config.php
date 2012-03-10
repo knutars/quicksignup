80798ec94a97e7d5f7f0c1421f41b9aa6d49283d
Knutars <krister@knutars.com>
2012-03-11 00:53:37 +0100

    SWE

--- config.php
+++ config.php
@@ -9,34 +9,30 @@ if ($_POST['update_menu']) {
 	$menu_pref['quicksignup'] = $_POST['pref'];
 	$tmp = addslashes(serialize($menu_pref));
 	$sql->db_Update("core", "e107_value='$tmp' WHERE e107_name='menu_pref' ");
-	$ns->tablerender("", "<div style=\'text-align:center\'><b>Settings updated successfully!</b></div>");
+	$ns->tablerender("", "<div style=\'text-align:center\'><b>Inställningarna uppdaterade!</b></div>");
 }
 
 $text = "
 	<div style='text-align:center'>
 	<form action='".e_SELF."?".e_QUERY."' method='post'>
 	<table style='width:85%' class='fborder' >
+
 	<tr>
-	<td style='width:30%' class='forumheader3'>Maximum Addition Integer<br /><i>1 + [this number] =</i></td>
+	<td style='width:30%' class='forumheader3'>Högsta tal att addera<br /><small>Ex 4 ger aldrig högre än 4 + 4</small></td>
 	<td style='width:70%' class='forumheader3'>
 	<input type='text' class='tbox' name='pref[maxinteger]' value='".(($menu_pref['quicksignup']['maxinteger']) ? $menu_pref['quicksignup']['maxinteger'] : "")."' />
 	</td>
 	</tr>
+
 	<tr>
-	<td style='width:30%' class='forumheader3'>Use CAPTCHA image verification?</td>
-	<td style='width:70%' class='forumheader3'>
-	<input type='checkbox' name='pref[captcha]' value='1' ".($menu_pref['quicksignup']['captcha'] == 1 ? " checked='checked'" : "")." />
-	</td>
-	</tr>
-	<tr>
-	<td colspan='2' class='forumheader' style='text-align: center;'><input class='button' type='submit' name='update_menu' value='Save Settings!' /></td>
+	<td colspan='2' class='forumheader' style='text-align: center;'><input class='button' type='submit' name='update_menu' value='Spara inställningarna!' /></td>
 	</tr>
 	</table>
 	</form>
 	</div>
 	";
 
-$ns->tablerender("Configure Quick Signup", $text);
+$ns->tablerender("Konfigurera snabbregistrering", $text);
 
 require_once(e_ADMIN."footer.php");
 
